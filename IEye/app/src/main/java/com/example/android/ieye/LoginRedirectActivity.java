8854bcdeef9c9f1e1907f4a674e13d20584f9529
Pavankumar1234 <lekkala.pavan19@gmail.com>
2018-07-14 19:57:18 +0530

    Volunteer signup

---
+++ IEye/app/src/main/java/com/example/android/ieye/LoginRedirectActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.ieye;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LoginRedirectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_redirect);
+    }
+
+    public void studentclick(View v)
+    {
+        Intent iStudent = new Intent(LoginRedirectActivity.this,MainActivity.class);
+        startActivity(iStudent);
+    }
+    public void volunteerclick(View v)
+    {
+        Intent iVolunteer = new Intent(LoginRedirectActivity.this,VolunteerSignupActivity.class);
+        startActivity(iVolunteer);
+    }
+}
